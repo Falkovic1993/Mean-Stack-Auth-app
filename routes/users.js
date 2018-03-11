018df512c1321f547fdabdc0a180ba65e9f185c6
Emil Falk Knudsen <emilfalkknudsen93@gmail.com>
2018-03-11 22:52:11 +0100

    Hash password + addUser added.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 // Register 
 router.post('/register', (req, res, next) => {
-    res.send('Register');
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if(err){
+            res.json({sucess:false, msg:'Failed to register user'});
+        } else {
+            res.json({sucess:true, msg:'User saved'});
+        }
+    })
+
 });
 
 // Authenticate
